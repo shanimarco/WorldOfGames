0ba04115f9ff4b002e0816439c1a35a9ddad5fa8
shanimarco <shanimarco77@gmail.com>
2024-05-25 17:29:44 +0300

    fix CurrencyRouletteGame func

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,4 +1,5 @@
-from forex_python.converter import CurrencyRates
+from forex_python.converter import CurrencyCodes
+import requests
 import random
 
 def get_guess_from_user(random_num):
@@ -8,11 +9,15 @@ def get_guess_from_user(random_num):
     return user_guess
 
 def get_money_interval(game_difficulty, random_num):
-    c = CurrencyRates()
-    currency_rate = c.get_rate('ILS', 'USD')
-    lowes_range = (int(random_num) / currency_rate) - (5 - int(game_difficulty))
-    highest_range = (int(random_num) / currency_rate) + (5 - int(game_difficulty))
+    url = 'https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_zPMBaKFhAi053GmZVx9KlW9FMJ8AZrf7HZS2hBbP&currencies=ILS'
+    response = requests.get(url)
+    currency_rate_json = response.json()
+    currency_rate = currency_rate_json['data']['ILS']
+    print (currency_rate)
+    lowes_range = (int(random_num) * currency_rate) - (5 - int(game_difficulty))
+    highest_range = (int(random_num) * currency_rate) + (5 - int(game_difficulty))
     interval = [int(lowes_range), int(highest_range)]
+    print(interval)
     return interval
 
 
